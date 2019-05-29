3bf2c5de5d026d33a8132872d8da4c8d136fc206
Javier Arias <javier@arias.re>
2019-05-29 14:09:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Altmetrics DOI regisration
+[![Build Status](https://travis-ci.org/hirmeos/altmetrics_registration.svg?branch=master)](https://travis-ci.org/hirmeos/altmetrics_registration) [![Release](https://img.shields.io/github/release/hirmeos/altmetrics_registration.svg?colorB=58839b)](https://github.com/hirmeos/altmetrics_registration/releases) [![License](https://img.shields.io/github/license/hirmeos/altmetrics_registration.svg?colorB=ff0000)](https://github.com/hirmeos/altmetrics_registration/blob/master/LICENSE)
+
+Obtain a list of DOIs and associated URLs from an instance of [hirmeos/identifier_translation_service][1] and register them with the [hirmeos/altmetrics][2] API.
+
+This software may be useful to people who have set up an Identifier Translation Service in order to run any of the usage metrics drivers documented in [https://metrics.operas-eu.org/docs/getting-started][3]. If you are **not** intending to use the translation service for anything else you should use a different approach to register DOIs with the altmetrics API.
+
+## Setup
+### Requirements
+An instance of [hirmeos/identifier_translation_service][1] - you must first have a running version of this API.
+
+An [altmetrics user account][4].
+
+## Run via crontab
+```
+0 0 * * 0 docker run --rm --name "altmetrics_registration" --env-file /path/to/config.env openbookpublishers/altmetrics_registration:1
+```
+
+[1]: https://github.com/hirmeos/identifier_translation_service "Identifier Translation Service"
+[1]: https://github.com/hirmeos/altmetrics "Altmetrics Service"
+[3]: https://metrics.operas-eu.org/docs/getting-started "Metrics docs"
+[4]: https://docs.metrics.ubiquity.press/metrics/index.html "Almetrics user account"
---
+++ src/client.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import base64
+import httplib2
+
+URI_API_USER = os.environ['URI_API_USER']
+URI_API_PASS = os.environ['URI_API_PASS']
+AUTH_API_ENDP = os.environ['AUTH_API_ENDP']
+URI_API_WORKS = os.environ['URI_API_WORKS']
+ALTMETRICS_AUTH_ENDP = os.environ['ALTMETRICS_AUTH_ENDP']
+ALTMETRICS_ENDP = os.environ['ALTMETRICS_ENDP']
+ALTMETRICS_USER = os.environ['ALTMETRICS_USER']
+ALTMETRICS_PASS = os.environ['ALTMETRICS_PASS']
+
+
+class IdentifiersClient(object):
+    """Single entry point to the translation service API"""
+
+    def __init__(self):
+        self.token = self.get_token(AUTH_API_ENDP, URI_API_USER, URI_API_PASS)
+        self.auth = 'Bearer ' + self.token
+        self.auth_headers = {'Authorization': self.auth}
+
+    def get_token(self, url, email, passwd):
+        h = httplib2.Http()
+        credentials = {'email': email, 'password': passwd}
+        headers = {'content-type': 'application/json'}
+        res, content = h.request(url, 'POST', json.dumps(credentials), headers)
+        if res.status != 200:
+            raise ValueError(content.decode('utf-8'))
+        return json.loads(content.decode('utf-8'))['data'][0]['token']
+
+    def request_identifiers(self, url):
+        h = httplib2.Http()
+        res, content = h.request(url, 'GET', headers=self.auth_headers)
+        if res.status != 200:
+            raise ValueError(content.decode('utf-8'))
+        return json.loads(content.decode('utf-8'))['data']
+
+    def get_all_works(self):
+        url = (URI_API_WORKS
+               + '?filter=uri_scheme:http,uri_scheme:https,uri_scheme:info:doi'
+               + '&strict=true')
+        return self.request_identifiers(url)
+
+
+class AltmetricsClient(object):
+    """Single entry point to the altmetrics API"""
+
+    def __init__(self):
+        self.token = self.get_token(ALTMETRICS_AUTH_ENDP, ALTMETRICS_USER,
+                                    ALTMETRICS_PASS)
+        self.auth = 'Bearer ' + self.token
+        self.auth_headers = {'Authorization': self.auth}
+
+    def get_token(self, url, email, passwd):
+        h = httplib2.Http()
+        auth = base64.b64encode(bytes('%s:%s' % (email, passwd), 'utf-8'))
+        headers = {'Authorization': 'Basic %s' % (auth)}
+        res, content = h.request(url, 'GET', headers)
+        if res.status != 200:
+            raise ValueError(content.decode('utf-8'))
+        return json.loads(content.decode('utf-8'))
+
+    def register_dois(self, data):
+        h = httplib2.Http()
+        res, content = h.request(ALTMETRICS_ENDP, 'POST', data,
+                                 headers=self.auth_headers)
+        if res.status != 200:
+            raise ValueError(content.decode('utf-8'))
+        return json.loads(content.decode('utf-8'))
